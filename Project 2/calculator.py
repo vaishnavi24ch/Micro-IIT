a82a3d1002ad1cecfcd0c81973072dec2410d6ed
vaishnavi24ch <vaishnavi24ch@gmail.com>
2025-03-16 06:01:17 +0000

    Add files via upload

---
+++ Project 2/calculator.py
@@ -0,0 +1,44 @@
+package calculator;
+import java.util.Scanner;
+
+public class Program {
+
+public static void main(String[] args) {
+char operator;
+double num1,num2;
+Scanner input = new Scanner(System.in);
+
+System.out.println("Enter the Operator (+, -, *, /)");
+operator = input.next().charAt(0);
+
+System.out.println("Enter the two numbers one by one");
+num1 = input.nextDouble();
+num2 = input.nextDouble();
+
+input.close();
+
+switch( operator ) {
+case '+':
+System.out.printf("%.2f + %.2f = %.2f", num1, num2, (num1+num2));
+break;
+    
+case '-':
+System.out.printf("%.2f %.2f %.2f", num1, num2, (num1-num2));
+break;
+    
+case '*':
+System.out.printf("%.2f %.2f %.2f", num1, num2, (num1*num2));
+break;
+    
+case '/':
+if(num2!=0)
+System.out.printf("%.2f / %.2f = %.2f", num1, num2, (num1/num2));
+else
+System.out.println("Divide by Zero Situation");
+break;
+  
+default:
+System.out.println("%c is an invalid operator",operator);
+    }
+  }
+}
